eae7708fe2d766a06a5ce14e6d5e1988ef4231c3
JohnBerkley <jberkley95@gmail.com>
2016-10-24 12:47:59 -0700

    Fixed Names/Buttons

--- app/src/main/java/com/example/fsdgeniusacadamy/poisonplants/OCActivity.java
+++ app/src/main/java/com/example/fsdgeniusacadamy/poisonplants/OCActivity.java
@@ -5,23 +5,53 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class OCActivity extends AppCompatActivity {
 
     ImageView poisonOak;
+    Button home, call, about;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_oc);
 
+        home = (Button) findViewById(R.id.oc_homeButton);
+        call = (Button) findViewById(R.id.oc_callButton);
+        about = (Button) findViewById(R.id.oc_aboutButton);
         poisonOak = (ImageView) findViewById(R.id.img_poisonoak);
+
         poisonOak.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://en.wikipedia.org/wiki/Toxicodendron_diversilobum"));
                 startActivity(browserIntent);
+            }
+        });
+
+        home.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainMenu.class);
+                startActivity(intent);
+            }
+        });
+
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), CallActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        about.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), AboutActivity.class);
+                startActivity(intent);
             }
         });
     }
